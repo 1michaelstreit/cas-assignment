ebc40d835ba2f6ccbf33d0b3382a7c0855abdc08
michaelstreit <micha.streit@bluewin.ch>
2025-04-02 10:10:09 +0200

    Assgiment 2

--- assignments/toolcalibration/calibration.py
+++ assignments/toolcalibration/calibration.py
@@ -18,7 +18,27 @@ def pivot_calibration(transforms):
 
     ## TODO: Implement pivot calibration as discussed in the lecture
 
+    A = []
+    b = []
+    I = np.eye(3)
+
+    for F in transforms:
+        Ri = F[:3, :3]   # extract rotation
+        pi = F[:3, 3]    # extract translation
+
+        A.append(np.hstack((Ri, -I)))
+        b.append(-pi)
+
+    A = np.vstack(A)
+    b = np.vstack(b).reshape(-1,1)
+
+    # use least square methode
+    p, _, _, _ = np.linalg.lstsq(A, b, rcond=None)  # Ri*pt -pp = pi -> solve pt,pp
+
+    pt, pp = p[:3], p[3:]
+
     T = np.eye(4)
+    T[:3, 3] = pt.flatten()
 
     return T
 
@@ -38,6 +58,8 @@ def calibration_device_calibration(camera_T_reference, camera_T_tool, reference_
 
     ## TODO: Implement a calibration method which uses a calibration device
 
-    T = np.eye(4)
+    tool_T_tip = np.linalg.inv(camera_T_tool) @ camera_T_reference @ reference_T_tip
+
+    T = tool_T_tip
     
     return T
